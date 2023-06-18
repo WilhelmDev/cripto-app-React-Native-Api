81f765c51bac556adf6a210a862f3b090147e633
WilhelmDev <wiljoseflores@gmail.com>
2023-06-18 12:08:44 -0400

    first steps

---
+++ src/components/FormCotizer.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function FormCotizer() {
+    return (
+        <View>
+            <Text>FormCotizer</Text>
+        </View>
+    )
+}
---
+++ src/components/Quotation.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Quotation() {
+    return (
+        <View>
+            <Text>Quotation</Text>
+        </View>
+    )
+}
