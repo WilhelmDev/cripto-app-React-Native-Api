3743f3831028a713ddecb3f84c44eb636ce65c17
WilhelmDev <wiljoseflores@gmail.com>
2023-06-19 08:07:28 -0400

    first steps with header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import { useFonts } from 'expo-font';
+import {} from 'expo'
 
 export default function Header() {
+
+    const [fontsLoaded] = useFonts({
+        'Lato-Black': require('../../assets/fonts/Lato-Black.ttf'),
+        'Lato-Regular': require('../../assets/fonts/Lato-Regular.ttf'),
+    });
+    if (!fontsLoaded) {//TODO Colocarlo en el app
+        return <Text>Loading</Text>
+    }
+
     return (
-        <View>
-            <Text>Header</Text>
-        </View>
+        <>
+            <Text style={styles.header}>Criptomonedas</Text>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    header:{
+        fontFamily: 'Lato-Black',
+        fontSize:20,
+        backgroundColor:'#5e49e2',
+        paddingTop: StatusBar.currentHeight,
+        paddingBottom:10,
+        textAlign:'center',
+        textTransform:'uppercase',
+        color:'#fff',
+        marginBottom:30
+    }
+})
